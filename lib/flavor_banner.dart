d84486592873e5c3a7f5338303b0391e3bd9b898
Muldec <34677746+muldec@users.noreply.github.com>
2019-12-27 15:25:17 +0100

    Exposes only the public API

--- lib/flavor_banner.dart
+++ lib/flavor_banner.dart
@@ -1,4 +1,6 @@
 library flavor_banner;
 
-export 'package:flavor_banner/components/flavor_banner.dart';
-export 'package:flavor_banner/components/flavor_config.dart';
+export 'package:flavor_banner/components/flavor_banner.dart'
+  show FlavorBanner;
+export 'package:flavor_banner/components/flavor_config.dart'
+  show Flavor, FlavorConfig;
